60ec7f029107478d02b16325440f6d04fd6fd4aa
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-14 17:24:02 +0900

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+    
       lowercase: true,
       trim: true,
       match: [EMAIL_REGEX, "유효한 이메일"]
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     passwordHash: {
       type: String,
       required: true,
-      minlength: 8
+    
     },
     displayName: {
       type: String,
