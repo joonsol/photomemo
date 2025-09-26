79f1e56d917ec9a9b4719d0cf1af3ea3bc23d472
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-26 16:52:51 +0900

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true,   // 이 포트가 이미 사용 중이면 실패(자동 증가 금지)
+    host: true          // 네트워크 접근 필요시
   }
 })
